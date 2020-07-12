101ec34ef54f7c668b6c4890113191d96835c0ce
baldore <acornssn@gmail.com>
2020-07-12 18:20:58 -0500

    Add server call to error case

--- src/initial-form/initial-form.test.js
+++ src/initial-form/initial-form.test.js
@@ -1,13 +1,37 @@
 import { getByLabelText, fireEvent, findByText } from '@testing-library/dom'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 import createForm from './initial-form'
 
-test('should initial structure', async () => {
+const server = setupServer()
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterEach(() => {
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  server.close()
+})
+
+test('should display an error message if there is a problem with the API', async () => {
+  server.use(
+    rest.post('http://api.sendagifttosomeone.com/gifts', (_req, res, ctx) =>
+      res(ctx.status(500), ctx.json({ message: 'Invalid object' }))
+    )
+  )
   const { container } = createForm()
-  const firstName = getByLabelText(container, 'First Name')
+  const firstName = getByLabelText(container, 'Email')
 
   fireEvent.change(firstName, { target: { value: 'Facundo' } })
   fireEvent.submit(container)
 
-  const errorMessage = await findByText(container, /there was an error/i)
+  const errorMessage = await findByText(
+    container,
+    /there was an error: invalid object/i
+  )
   expect(errorMessage).toMatchSnapshot()
 })
