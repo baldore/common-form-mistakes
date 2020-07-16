ea9cadb7bea808074738371926476fdc2e7712dd
baldore <acornssn@gmail.com>
2020-07-16 11:43:48 -0500

    Add analytics

--- src/initial-form/initial-form.test.js
+++ src/initial-form/initial-form.test.js
@@ -5,17 +5,12 @@ import createForm from './initial-form'
 
 const server = setupServer()
 
-beforeAll(() => {
-  server.listen()
+beforeAll(() => server.listen())
+beforeEach(() => {
+  window.dataLayer = []
 })
-
-afterEach(() => {
-  server.resetHandlers()
-})
-
-afterAll(() => {
-  server.close()
-})
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 test('should display an error message if there is a problem with the API', async () => {
   server.use(
@@ -34,13 +29,27 @@ test('should display an error message if there is a problem with the API', async
     /there was an error: invalid object/i
   )
   expect(errorMessage).toMatchSnapshot()
+
+  expect(window.dataLayer).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "error": "Invalid object",
+        "formId": "gift",
+      },
+    ]
+  `)
 })
 
 test('should display a success message', async () => {
   server.use(
-    rest.post('http://api.sendagifttosomeone.com/gifts', (_req, res, ctx) =>
-      res(ctx.status(200), ctx.json({ status: 'ok' }))
-    )
+    rest.post('http://api.sendagifttosomeone.com/gifts', (req, res, ctx) => {
+      expect(req.body).toMatchInlineSnapshot(`
+        Object {
+          "email": "Facundo",
+        }
+      `)
+      return res(ctx.status(200), ctx.json({ status: 'ok' }))
+    })
   )
   const { container } = createForm()
   const firstName = getByLabelText(container, 'Email')
@@ -50,4 +59,13 @@ test('should display a success message', async () => {
 
   const success = await findByText(container, /form sent successfully/i)
   expect(success).toMatchSnapshot()
+
+  expect(window.dataLayer).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "formId": "gift",
+        "success": true,
+      },
+    ]
+  `)
 })
