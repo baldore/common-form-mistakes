457e96512b3512c3bc389ca0c712eb7b7c7f697b
baldore <acornssn@gmail.com>
2020-07-16 09:00:45 -0500

    Add successful message

--- src/initial-form/initial-form.test.js
+++ src/initial-form/initial-form.test.js
@@ -35,3 +35,19 @@ test('should display an error message if there is a problem with the API', async
   )
   expect(errorMessage).toMatchSnapshot()
 })
+
+test('should display a success message', async () => {
+  server.use(
+    rest.post('http://api.sendagifttosomeone.com/gifts', (_req, res, ctx) =>
+      res(ctx.status(200), ctx.json({ status: 'ok' }))
+    )
+  )
+  const { container } = createForm()
+  const firstName = getByLabelText(container, 'Email')
+
+  fireEvent.change(firstName, { target: { value: 'Facundo' } })
+  fireEvent.submit(container)
+
+  const success = await findByText(container, /form sent successfully/i)
+  expect(success).toMatchSnapshot()
+})
